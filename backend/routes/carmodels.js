9ab18601f8f04918e4eae49ab15c00afb858ae00
Axel-Hellman <axel.hellman@outlook.com>
2020-01-24 14:12:39 +0100

    lade till kommentarer

--- backend/routes/carmodels.js
+++ backend/routes/carmodels.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 let Carmodel = require("../models/carmodels.model");
 
+//hittar alla carmodels från MongoDB-Atlas databasen, returnerar sedan i .json format
 router.route("/").get((req, res) => {
-  Carmodel.find() //hittar alla bilmodeller på MongoDB-Atlas databasen
-    .then(carmodels => res.json(carmodels)) //returnerar sedan bilmodeller i .json format
+  Carmodel.find()
+    .then(carmodels => res.json(carmodels))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+//Skapar ny carmodel: samlar först all data som behövs, skapar en ny bilmodell och sparar den i databasen
 router.route("/add").post((req, res) => {
   const id = Number(req.body.id);
   const brand = req.body.brand;
@@ -26,12 +28,15 @@ router.route("/add").post((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+//Hittar en bilmodell med hjälp av mongoDBs automatiskt genererade, unika, "_id" och returnerar den
 router.route("/:id").get((req, res) => {
   Carmodel.findById(req.params.id)
     .then(carmodels => res.json(carmodels))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+//Hittar först en bilmodell med hjälp av mongoDBs automatiskt genererade, unika, "_id" och returnerar den
+//Raderar sedan samma bilmodell
 router.route("/:id").delete((req, res) => {
   Carmodel.findById(req.params.id)
     .then(carmodels => res.json(carmodels))

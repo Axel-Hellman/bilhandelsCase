6bd5eac227d7143970051baf7ebec49c5330aae0
Axel-Hellman <axel.hellman@outlook.com>
2020-01-24 18:07:06 +0100

    added data.json and started on sales.js

--- backend/routes/sales.js
+++ backend/routes/sales.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
 let Sale = require("../models/sales.model");
+let Employee = require("../models/employees.model");
+let Carmodel = require("../models/carmodels.model");
+
+router.route("/total_sales").get((req, res) => {});
 
 module.exports = router;

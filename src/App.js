7d2a0157bdac75010163ef121a71233da6f8564e
Axel-Hellman <axel.hellman@outlook.com>
2020-01-25 17:06:52 +0100

    removed autogenerated logo code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 
 //importerar alla components
